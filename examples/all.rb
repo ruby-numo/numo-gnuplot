b6f2167bfe2b02f9851c96835efa23b0c7d01ced
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-05-23 00:26:07 +0900

    add examples/all.rb

---
+++ examples/all.rb
@@ -0,0 +1,21 @@
+#require "numo/gnuplot"
+require "../lib/numo/gnuplot"
+
+gp = Numo.gnuplot
+
+puts "mouse/key on window to continue"
+Dir.glob("ex*.rb") do |frb|
+  gp.reset
+  load frb
+  gp.pause mouse:"any"
+end
+
+gp.set term:"png"
+
+Dir.glob("ex*.rb") do |frb|
+  fimg = File.basename(frb,".rb")+".png"
+  gp.reset
+  gp.set output:fimg
+  load frb
+  puts "wrote #{fimg}"
+end
