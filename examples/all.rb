227bd050e3f59965b84ccd688c14da64b53622dd
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-06-11 22:14:01 +0900

    minor fix in examples

--- examples/all.rb
+++ examples/all.rb
@@ -1,17 +1,22 @@
 require "numo/gnuplot"
+#require_relative "../lib/numo/gnuplot"
 
 gp = Numo.gnuplot
+#gp.debug_on
 
-puts "mouse/key on window to continue"
-Dir.glob("ex*.rb") do |frb|
+puts "enter key to continue"
+
+files = Dir.glob("ex*.rb").sort
+files.each do |frb|
   gp.reset
+  puts "*** "+frb+" ***"
   load frb
-  gp.pause mouse:"any"
+  gets
 end
 
 gp.set term:"png"
 
-Dir.glob("ex*.rb") do |frb|
+files.each do |frb|
   fimg = File.basename(frb,".rb")+".png"
   gp.reset
   gp.set output:fimg
--- examples/ex010.rb
+++ examples/ex010.rb
@@ -1,7 +1,7 @@
 require "numo/narray"
 
 # 3D plot of 2D data with Numo::NArray
-n = 60
+n = 120
 x = (Numo::DFloat.new(1,n).seq/n-0.5)*30
 y = (Numo::DFloat.new(n,1).seq/n-0.5)*30
 r = Numo::NMath.sqrt(x**2+y**2) + 1e-10
@@ -9,6 +9,5 @@ z = Numo::NMath.sin(r)/r
 
 Numo.gnuplot do
   set title:'3D plot of 2D data'
-  set dgrid3d:[60,60]
   splot z, w:'pm3d', t:'sin(r)/r'
 end
